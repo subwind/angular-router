a20860189fc0becf397d76978d1e41b476283bf6
subwind <subwind@gmail.com>
2019-06-24 18:05:35 +0800

    3 files modified, 3 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: AppComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import {AppRoutingModule} from './app-routing.module'
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,AppRoutingModule ],
-  declarations: [ AppComponent, HelloComponent, Page1Component, Page2Component ],
+  declarations: [ AppComponent, HelloComponent,HomeComponent, Page1Component, Page2Component ],
   bootstrap:    [ AppComponent ],
   providers:[{ provide: APP_BASE_HREF, useValue : '/' }]
 })
