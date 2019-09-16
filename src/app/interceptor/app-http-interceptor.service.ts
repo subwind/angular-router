550c913d5c331f224ed8f837be447814df393263
subwind <subwind@gmail.com>
2019-09-17 00:14:29 +0800

    1 file modified

--- src/app/interceptor/app-http-interceptor.service.ts
+++ src/app/interceptor/app-http-interceptor.service.ts
@@ -12,7 +12,7 @@ import { Observable, throwError } from 'rxjs';
 
 @Injectable()
 export class AppHttpInterceptorService implements HttpInterceptor {
-
+  //https://wellwind.idv.tw/blog/2019/01/06/mastering-angular-31-how-to-create-new-http-client-and-ignore-http-interceptors/
   constructor() { }
 
   public intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
